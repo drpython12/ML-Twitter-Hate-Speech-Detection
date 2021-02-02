f02a76728442514b50b4681dee2e2190b95e4149
drpython12 <gandhip@britishschoolmuscat.com>
2021-02-02 11:26:07 +0400

    2/2/2021 11:25

--- Classifier.py
+++ Classifier.py
@@ -53,15 +53,15 @@ def PPT(df):
 
     # Removing numbers
     df["tweet"] = df["tweet"].str.replace("[{}]".format(string.digits), "")
+
+    # Stemming each word of the tweets
+    stemmer = PorterStemmer()
+    df["parsed tweet"] = df["tweet"].apply(lambda x: "" "".join([stemmer.stem(word) for word in x.split()])) 
     
     # Tokenize each word
     print(df["tweet"])
     df["tweet"] = df["tweet"].apply(lambda x: nltk.WordPunctTokenizer().tokenize(x))
     print(df["tweet"])
-
-    # Stemming each word of the tweets
-    stemmer = PorterStemmer()
-    df["parsed tweet"] = df["tweet"].apply(lambda x: "" "".join([stemmer.stem(word) for word in x.split()])) 
 
 # Word cloud generator based on the parsed data
 def Wordcloud(df): 
