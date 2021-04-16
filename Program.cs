5d5e9168a54c7aa3c69892534a70e3ec70533307
drpython12 <gandhip@britishschoolmuscat.com>
2021-04-16 14:07:38 +0400

    16/4/2021 14:10

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ML_Twitter_Hate_Speech_Detection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Hello, " + name + ", nice to meet you");  
+        }
+    }
+}
