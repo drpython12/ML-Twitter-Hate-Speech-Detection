843a0c0c6c68f19975e272627190f35bc81f1d97
drpython12 <gandhip@britishschoolmuscat.com>
2021-04-24 12:30:14 +0400

    24/4/2021 12:30

--- Streamer.py
+++ Streamer.py
@@ -9,23 +9,28 @@ access_token_secret = "GLQpVUkMowiePQlGnPDfTYJyWjwjbvqOLX8JO9LhBihTX"
 consumer_key = "IwlYQW5mIhCeMeSpcdwF8DUzS"
 consumer_key_secret = "InwCsZzwGKYveB8fVboY58MQXvWu3Xa9qxxp7Nt0Nieo9EoWGk"
 
+global n_tweets
+n_tweets = int(input("Number of tweets to stream: "))
+
+
 class Listener(StreamListener):
 
     def on_status(self, status):
-        print(status.text)
-
-        if status.lang == "en":
-
-            if hasattr(status, "extended_tweet"):
-                tweet_text = status.extended_tweet["full_text"]
-            else:
-                tweet_text = status.text
-
-            if hasattr(status, "retweeted_status") == False:
-
-                with open("imported_tweets.csv", "a", encoding="utf-8") as file:
-                    file.write("%s, %s, %s, %s, %s,\n" % ("User ID INT", "User ID STR", "Name", "Display Name", "Tweet"))
-                    file.write("%d, %s, %s, %s, %s,\n" % (status.user.id, status.user.id_str, status.user.name, status.user.screen_name, tweet_text))
+        tweet_count = 0
+        if n_tweets >= tweet_count:
+            print(status.text)
+            tweet_count = tweet_count + 1
+            if status.lang == "en":
+                if hasattr(status, "extended_tweet"):
+                    tweet_text = status.extended_tweet["full_text"]
+                else:
+                    tweet_text = status.text
+                if hasattr(status, "retweeted_status") == False:
+                    with open("imported_tweets.csv", "a", encoding="utf-8") as file:
+                        file.write("%s, %s, %s, %s, %s,\n" % ("User ID INT", "User ID STR", "Name", "Display Name", "Tweet"))
+                        file.write("%d, %s, %s, %s, %s,\n" % (status.user.id, status.user.id_str, status.user.name, status.user.screen_name, tweet_text))
+        else:
+            stream.disconnect()
 
     def on_error(self, status_code):
         print("Streaming error", status_code)
