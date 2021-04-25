46cab6652f4624396399849146c94eb361d2836f
drpython12 <gandhip@britishschoolmuscat.com>
2021-04-25 13:46:22 +0400

    25/4/2021 13:45

--- Streamer.py
+++ Streamer.py
@@ -9,17 +9,12 @@ access_token_secret = "GLQpVUkMowiePQlGnPDfTYJyWjwjbvqOLX8JO9LhBihTX"
 consumer_key = "IwlYQW5mIhCeMeSpcdwF8DUzS"
 consumer_key_secret = "InwCsZzwGKYveB8fVboY58MQXvWu3Xa9qxxp7Nt0Nieo9EoWGk"
 
-global n_tweets
-n_tweets = int(input("Number of tweets to stream: "))
-
-
 class Listener(StreamListener):
 
-    def on_status(self, status):
-        tweet_count = 0
-        if n_tweets >= tweet_count:
+    for i in range(0, int(input("Number of tweets to stream: "))):
+        i = i + 1
+        def on_status(self, status):
             print(status.text)
-            tweet_count = tweet_count + 1
             if status.lang == "en":
                 if hasattr(status, "extended_tweet"):
                     tweet_text = status.extended_tweet["full_text"]
@@ -29,8 +24,8 @@ class Listener(StreamListener):
                     with open("imported_tweets.csv", "a", encoding="utf-8") as file:
                         file.write("%s, %s, %s, %s, %s,\n" % ("User ID INT", "User ID STR", "Name", "Display Name", "Tweet"))
                         file.write("%d, %s, %s, %s, %s,\n" % (status.user.id, status.user.id_str, status.user.name, status.user.screen_name, tweet_text))
-        else:
-            stream.disconnect()
+            else:
+                stream.disconnect()
 
     def on_error(self, status_code):
         print("Streaming error", status_code)
