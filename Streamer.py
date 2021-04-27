69172178cdb5805ad7526bfb145f4525f2cb559b
drpython12 <gandhip@britishschoolmuscat.com>
2021-04-27 11:08:24 +0400

    27/4/2021 11:10

--- Streamer.py
+++ Streamer.py
@@ -11,32 +11,49 @@ consumer_key_secret = "InwCsZzwGKYveB8fVboY58MQXvWu3Xa9qxxp7Nt0Nieo9EoWGk"
 
 class Listener(StreamListener):
 
-    for i in range(0, int(input("Number of tweets to stream: "))):
-        i = i + 1
-        def on_status(self, status):
+    # Method to obtain tweets as well as relavent metadata
+    def on_status(self, status):
+        # Ensures that only tweets in English are obtained
+        if status.lang == "en":
             print(status.text)
-            if status.lang == "en":
-                if hasattr(status, "extended_tweet"):
-                    tweet_text = status.extended_tweet["full_text"]
-                else:
-                    tweet_text = status.text
-                if hasattr(status, "retweeted_status") == False:
-                    with open("imported_tweets.csv", "a", encoding="utf-8") as file:
-                        file.write("%s, %s, %s, %s, %s,\n" % ("User ID INT", "User ID STR", "Name", "Display Name", "Tweet"))
-                        file.write("%d, %s, %s, %s, %s,\n" % (status.user.id, status.user.id_str, status.user.name, status.user.screen_name, tweet_text))
+            # Checks if tweet is extended and if the condition is met, the full tweet is obtained
+            if hasattr(status, "extended_tweet"):
+                tweet_text = status.extended_tweet["full_text"]
             else:
-                stream.disconnect()
+                # Tweet is regular length
+                tweet_text = status.text
+            # Opens CSV file to write tweet and metadata
+            with open("imported_tweets.csv", "a", encoding="utf-8") as file:
+                file.write("%s, %s, %s, %s, %s,\n" % ("User ID INT", "User ID STR", "Name", 
+                "Display Name", "Tweet"))
+                file.write("%d, %s, %s, %s, %s,\n" % (status.user.id, status.user.id_str, 
+                status.user.name, status.user.screen_name, tweet_text))
 
+    # Method to identify and return any error messages to the user
     def on_error(self, status_code):
+        # Error code presented to user
         print("Streaming error", status_code)
         
 if __name__ == "__main__":
     
+    # Instantiates 'Listener' class
     listener = Listener()
 
+    # Provides the consumer keys to the OAuthHandler function
     auth = OAuthHandler(consumer_key, consumer_key_secret)
+    # Provides access tokens
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
 
+    # Creates a list to store filters in
+    track_list = []
+    # Asks the user how many filters to use
+    num_elements = int(input("How many filters would you like to enter in the list? "))
+    for i in range(0, num_elements):
+        # Prompts user for filter the number of times they input
+        track_list.append(str(input("Element: ")))
+
+    # Creates stream and provides neccessary inputs
     stream = Stream(auth=api.auth, listener=listener, tweet_mode="extended")
-    stream.filter(track=["trump"])
+    # Filters stream of tweets to match user input
+    stream.filter(track=track_list)
