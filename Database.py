30da7e4e3163c0cd37ef7a23c6f8c705f1251367
Palash Gandhi <gandhipalash0@gmail.com>
2021-05-18 14:50:05 +0400

    18/5/2021 14:50

--- Database.py
+++ Database.py
@@ -1,3 +1,4 @@
+from os import times, write
 import pyodbc
 import sys
 import csv
@@ -18,15 +19,62 @@ def WriteServer(writeFile):
     # Obtaining input fields for 'Tweet' table
     writeFile["TII"] = writeFile["Tweet ID Integer"]
     writeFile["TIS"] = writeFile["Tweet ID String"]
+    writeFile["TT"] = writeFile["tweet"]
 
-    # Iterating through each row in every column and inserting fields into database
-    for row in df.itertuples():
+    # Obtaining input fields for 'TweetEntities' table
+    writeFile["CA"] = writeFile["Date Created"]
+    writeFile["IRUI"] = writeFile["Reply User ID Integer"]
+    writeFile["IRUS"] = writeFile["Reply User ID String"]
+    writeFile["IRUN"] = writeFile["Reply User Screen Name"]
+    writeFile["LC"] = writeFile["Like Count"]
+    writeFile["RC"] = writeFile["Retweet Count"]
+    writeFile["CN"] = writeFile["Place"]
+
+    # Iterating through each row in every column and inserting fields into 'User' table
+    for row in writeFile.itertuples():
         cursor.execute('''INSERT INTO User VALUES (?, ?, ?, ?)''', 
         row.UII,
         row.UIS,
         row.NOU,
         row.DN
         )
+
+    # Insert query for 'Tweet' table
+    for row in writeFile.itertuples():
+        cursor.execute('''INSERT INTO Tweet VALUES (?, ?, ?, ?)''',
+        row.TII,
+        row.TIS,
+        row.TT,
+        row.UII
+        )
+
+    for row in writeFile.itertuples():
+
+        if row.IRUI != 'Null':
+
+        # Insert query for 'TweetEntities' table
+            cursor.execute('''INSERT INTO TweetEntities VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+            row.TII,
+            row.CA,
+            row.IRUI,
+            row.IRUS,
+            row.IRUN,
+            row.CN,
+            row.CN,
+            row.LC,
+            row.RC,
+            )
+        
+        else:
+
+            cursor.execute('''INSERT INTO TweetEntities (tweet_id, created_at, country, city_state, favourite_count, retweet_count) VALUES (?, ?, ?, ?, ?, ?)''',
+            row.TII,
+            row.CA,
+            row.CN,
+            row.CN,
+            row.LC,
+            row.RC
+            )
 
     cnxn.commit()
 
@@ -36,7 +84,7 @@ def ReadServer():
     cnxn = pyodbc.connect('DRIVER={Devart ODBC Driver for MySQL}; User ID=root; Password=Palashg12; Server=' + server + '; Database=' + database)
     cursor = cnxn.cursor()
 
-    query = "SELECT * FROM User"
+    query = "SELECT * FROM User, Tweet, TweetEntities"
     cursor.execute(query)
     row = cursor.fetchone()
     while row:
@@ -44,12 +92,6 @@ def ReadServer():
         row = cursor.fetchone()
 
 if __name__ == '__main__':
-
-    # Obtaining filename from UI's input to console which is based on user's selected file in dialog
-    filename = sys.argv[1]
-
-    # Reading input file as CSV
-    df = pd.read_csv(filename)
 
     # Declaring driver to use for connection
     driver = 'Devart ODBC Driver for MySQL'
@@ -60,4 +102,15 @@ if __name__ == '__main__':
     # Declaring database to query
     database = 'TwitterMachineLearningDatabase'
 
-    WriteServer(df)
+    if sys.argv[1] == "w":
+
+        # Obtaining filename from UI's input to console which is based on user's selected file in dialog
+        filename = sys.argv[2]
+
+        # Reading input file as CSV
+        df = pd.read_csv(filename)
+
+        WriteServer(df)
+    
+    elif sys.argv[1] == "r":
+        ReadServer()
